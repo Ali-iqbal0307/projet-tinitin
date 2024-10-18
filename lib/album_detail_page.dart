1ac6c1d94defa69a167907d41e100e30ddfd786a
ali-iqbal0307 <aliiqbal1997@hotmail.com>
2024-10-18 16:05:43 +0200

    padding sur la page details

--- lib/album_detail_page.dart
+++ lib/album_detail_page.dart
@@ -21,109 +21,118 @@ class AlbumDetailPage extends StatelessWidget {
         backgroundColor: Colors.redAccent,
       ),
       body: SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 16),
-            Text(
-              'Album n°${album.numero}',
-              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-            ),
-            const SizedBox(height: 16),
-            Text(
-              'Année de parution : ${album.year}',
-              style: const TextStyle(fontSize: 18),
-            ),
-            if (album.yearInColor != null)
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 4.0), // Padding réduit
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center, // Changez l'alignement ici
+            children: [
+              const SizedBox(height: 8),
               Text(
-                'Année en couleur : ${album.yearInColor}',
-                style: const TextStyle(fontSize: 18),
+                'Album n°${album.numero}',
+                style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold), // Taille de texte réduite
+                textAlign: TextAlign.center, // Centre le texte
               ),
-            const SizedBox(height: 16),
-            Text(
-              'Résumé : ${album.resume}',
-              style: const TextStyle(fontSize: 16),
-            ),
-            const SizedBox(height: 16),
-            Text(
-              'Location : ${album.location}',
-              style: const TextStyle(fontSize: 16),
-            ),
-            Center(
-              child: Image.asset(
-                album.image,
-                width: 400,
-                height: 400,
-                fit: BoxFit.cover,
+              const SizedBox(height: 8),
+              Text(
+                'Année de parution : ${album.year}',
+                style: const TextStyle(fontSize: 16),
+                textAlign: TextAlign.center, // Centre le texte
               ),
-            ),
-            const SizedBox(height: 16),
-            // --- Section pour afficher la carte ---
-            Container(
-              height: 300,
-              padding: const EdgeInsets.all(16),
-              child: FlutterMap(
-                options: MapOptions(
-                  initialCenter: LatLng(album.gps.latitude, album.gps.longitude), // Centre la carte sur les coordonnées de l'album
-                  initialZoom: 9.0,
+              if (album.yearInColor != null)
+                Text(
+                  'Année en couleur : ${album.yearInColor}',
+                  style: const TextStyle(fontSize: 16),
+                  textAlign: TextAlign.center, // Centre le texte
                 ),
-                children: [
-                  TileLayer(
-                    urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // Source de la carte
-                    userAgentPackageName: 'com.example.app',
+              const SizedBox(height: 8),
+              Text(
+                'Résumé : ${album.resume}',
+                style: const TextStyle(fontSize: 14), // Taille de texte réduite
+                textAlign: TextAlign.center, // Centre le texte
+              ),
+              const SizedBox(height: 8),
+              Text(
+                'Location : ${album.location}',
+                style: const TextStyle(fontSize: 14), // Taille de texte réduite
+                textAlign: TextAlign.center, // Centre le texte
+              ),
+              Center(
+                child: SizedBox(
+                  width: 300, // Largeur fixe pour l'image
+                  child: Image.asset(
+                    album.image,
+                    fit: BoxFit.cover,
                   ),
-                  RichAttributionWidget(
-                    attributions: [
-                      TextSourceAttribution(
-                        'OpenStreetMap contributors',
-                        onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')), // Lien vers les droits d'attribution
+                ),
+              ),
+              const SizedBox(height: 8),
+              // --- Section pour afficher la carte ---
+              Center( // Centrer la carte
+                child: SizedBox(
+                  height: 400, // Hauteur réduite pour la carte
+                  width: 300, // Largeur fixe pour la carte
+                  child: FlutterMap(
+                    options: MapOptions(
+                      initialCenter: LatLng(album.gps.latitude, album.gps.longitude), // Centre la carte sur les coordonnées de l'album
+                      initialZoom: 9.0,
+                    ),
+                    children: [
+                      TileLayer(
+                        urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // Source de la carte
+                        userAgentPackageName: 'com.example.app',
+                      ),
+                      RichAttributionWidget(
+                        attributions: [
+                          TextSourceAttribution(
+                            'OpenStreetMap contributors',
+                            onTap: () => launchUrl(Uri.parse('https://openstreetmap.org/copyright')), // Lien vers les droits d'attribution
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 16),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  context.read<ReadingListProvider>().toggleAlbum(album);
-                },
-                style: ElevatedButton.styleFrom(
-                  padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
-                  backgroundColor: isInReadingList
-                      ? const Color.fromARGB(255, 243, 61, 61)
-                      : Colors.white,
-                  side: const BorderSide(
-                      color: Color.fromARGB(255, 243, 61, 61), width: 2),
                 ),
-                child: Row(
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    Icon(
-                      isInReadingList ? Icons.bookmark_remove : Icons.bookmark_add,
-                      color: isInReadingList
-                          ? Colors.white
-                          : const Color.fromARGB(255, 243, 61, 61),
-                    ),
-                    const SizedBox(width: 8),
-                    Text(
-                      isInReadingList ? 'Retirer' : 'Ajouter',
-                      style: TextStyle(
+              ),
+              const SizedBox(height: 8),
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    context.read<ReadingListProvider>().toggleAlbum(album);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8), // Padding réduit pour le bouton
+                    backgroundColor: isInReadingList
+                        ? const Color.fromARGB(255, 243, 61, 61)
+                        : Colors.white,
+                    side: const BorderSide(
+                        color: Color.fromARGB(255, 243, 61, 61), width: 1), // Bordure réduite
+                  ),
+                  child: Row(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Icon(
+                        isInReadingList ? Icons.bookmark_remove : Icons.bookmark_add,
                         color: isInReadingList
                             ? Colors.white
                             : const Color.fromARGB(255, 243, 61, 61),
                       ),
-                    ),
-                  ],
+                      const SizedBox(width: 4),
+                      Text(
+                        isInReadingList ? 'Retirer' : 'Ajouter',
+                        style: TextStyle(
+                          color: isInReadingList
+                              ? Colors.white
+                              : const Color.fromARGB(255, 243, 61, 61),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
-  
-  launchUrl(Uri parse) {}
 }
